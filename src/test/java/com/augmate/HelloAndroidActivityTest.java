919bb3620a7a7cd5fe8e402d2ffab676f9bebee1
Johny Urgiles <johny@augmate.com>
2014-07-23 15:45:05 -0400

    Add empty unit test

---
+++ src/test/java/com/augmate/HelloAndroidActivityTest.java
@@ -0,0 +1,15 @@
+package com.augmate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class HelloAndroidActivityTest {
+
+    @Test
+    public void should() {
+
+    }
+
+}
